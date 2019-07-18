2e973ee21bf738eedec6a82b961bc1ba8283c86a
nicholas.santos <nicholas.santos@DIT3113US>
2019-07-18 09:26:39 -0700

    Working 1.1.0

--- Dungeons_and_Dragons_TextGame_Tomás_and_Nick/Nicholas_Sandbox.h
+++ Dungeons_and_Dragons_TextGame_Tomás_and_Nick/Nicholas_Sandbox.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <stdlib.h>
+#include <string>
+using namespace std;
 
 
  void AttackSystem();
@@ -23,28 +25,37 @@ enum EnemyType
 
 struct tempplayer
 {
+	bool Playerisdead = false;
 	int PlayerHP         = 0;
 	int PlayerArmor      = 0;
 	int PlayerHitChance  = 0;
 	int PlayerHitDamage  = 0;
+	int PlayerAddHitDamage = 0;
 	int PlayerCritChance = 0;
 	int PlayerCritDamage = 0;
-	int PlayerDodge      = 0;
-	bool Playerisdead = false;
-	int PlayerAddHitDamage = 0;
 	int PlayerAddCritDamage = 0;
+	int PlayerDodge      = 0;
+	string PlayerSkill1;
+	string PlayerSkill2;
+	string PlayerSkill3;
+	int PlayerSkill1Damage;
+	int PlayerSkill2Damage;
+	int PlayerSkill3Damage;
+
 	PlayerType playerType;
 };
 
 struct tempenemy
 {
+	bool Enemyisdead = false;
 	int EnemyHP          = 0;
 	int EnemyArmor       = 0;
 	int EnemyHitChance   = 0;
 	int EnemyHitDamage   = 0;
+	int EnemyDamage = 0;
 	int EnemyCritChance  = 0;
 	int EnemyCritDamage  = 0;
+	int EnemyAddCritDamage = 0;
 	int EnemyDodge       = 0;
-	bool Enemyisdead = false;
 	EnemyType enemyType;
 };
